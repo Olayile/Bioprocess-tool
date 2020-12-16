a160c6c47314fe839dfee00dc4d4ccab5f39c787
olayile <OLAYILEEJEKWU@GMAIL.COM>
2020-12-16 19:29:55 +0200

    Studentized residuals added in ols.py

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import SessionState
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+import plotly.express as px
+import pandas as pd
+import ols
+
+
+
+
+
 st.set_page_config(layout="wide")
 # st.markdown("""
 # <style>
@@ -30,106 +42,185 @@ local_css("style.css")
 
 
 #sidebar
-
-st.sidebar.selectbox('Biological Kinetics', ['Monod', 'Substrate Inhibition', 'Product Inhibition'])
-
-
-initial_conditions=st.sidebar.number_input('Initial Substrate')
-units_substrate = st.sidebar.selectbox('Units',['mg/L', 'g/L', 'mg/m3','g/m3'])
-# remember to change unit during analysis
-
-initial_conditions_Product =st.sidebar.number_input('Initial Product')
-units_product = st.sidebar.selectbox('Units',['mg/L', 'g/L', 'mg/m3','g/m3'], key=1)
+def main():
+    st.sidebar.selectbox('Biological Kinetics', ['Monod', 'Substrate Inhibition', 'Product Inhibition'])
 
 
-bioreactor=st.sidebar.selectbox('Bioreactor setup', ['Batch', 'Continous'])
-#If the batch operation is chosen, the physical parameters such as flow rate, volume,dilution rate, retention time are hidden and cannot be edited by the user
-if bioreactor == 'Batch':
-    st.image('Images/batch.png')
-else:
-    st.image('Images/continuous.png')
+    initial_conditions=st.sidebar.number_input('Initial Substrate')
+    units_substrate = st.sidebar.selectbox('Units',['mg/L', 'g/L', 'mg/m3','g/m3'])
+    # remember to change unit during analysis
 
-col1,col2,col3 = st.beta_columns(3)
-col1.selectbox('Reactant', ['Glucose', 'Xylose', 'Fructose', 'Other'])
-
-col2.image('Images/reaction.png', width= 200)
+    initial_conditions_Product =st.sidebar.number_input('Initial Product')
+    units_product = st.sidebar.selectbox('Units',['mg/L', 'g/L', 'mg/m3','g/m3'], key=1)
 
 
-col3.selectbox('Product', ['Organic Acids', 'Ethanol', 'Hydrogen', 'Other'])
+    bioreactor=st.sidebar.selectbox('Bioreactor setup', ['Batch', 'Continous'])
+    #If the batch operation is chosen, the physical parameters such as flow rate, volume,dilution rate, retention time are hidden and cannot be edited by the user
+    if bioreactor == 'Batch':
+        st.image('Images/batch.png')
+    else:
+        st.image('Images/continuous.png')
+
+    col1,col2,col3 = st.beta_columns(3)
+    col1.selectbox('Reactant', ['Glucose', 'Xylose', 'Fructose', 'Other'])
+
+    col2.image('Images/reaction.png', width= 200)
+
+
+    col3.selectbox('Product', ['Organic Acids', 'Ethanol', 'Hydrogen', 'Other'])
+        
+    session_state = SessionState.get(name="", button_sent=False)
+    button_sent = st.button('Click here to enter your data')
+
+    if button_sent:
+        session_state.button_sent = True
+
+    if session_state.button_sent:
     
 
-if st.button('Click here to enter your data'):
-
-    with st.beta_expander('Operation parameters'):
-        col1, col2, col3 = st.beta_columns(3)
-        col1.markdown('')
-        col1.markdown("<div> <span class='highlight blue'>Simulation Time <span class='bold'></div>", unsafe_allow_html=True)
-        col2.number_input('')
-        col3.selectbox('',['hours', 'minutes'])
-
-    #operating conditions
-        col1_1, col2_1, col3_1 = st.beta_columns(3)
-        col1_1.markdown('')
-        col1_1.markdown("<div> <span class='highlight blue'>Steptime, deltaT <span class='bold'></div>", unsafe_allow_html=True)
-        col2_1.number_input('', key=1)
-        col3_1.selectbox('',['hours', 'minutes'], key=1)
-    
-    #Initial concentration
-    with st.beta_expander('Initial concentrations'):
-        col1_2, col2_2, col3_2 = st.beta_columns(3)
-        col1_2.markdown('')
-        col1_2.markdown("<div> <span class='highlight blue'>Biomass concentration<span class='bold'></div>", unsafe_allow_html=True)
-        col2_2.number_input('', key=2)
-        col3_2.selectbox('',['mg/L', 'g/L', 'mg/m3','g/m3'])
-
-        ## substrate concenration
-
-        col1_3, col2_3, col3_3 = st.beta_columns(3)
-        col1_3.markdown('')
-        col1_3.markdown("<div> <span class='highlight blue'>Substrate concentration<span class='bold'></div>", unsafe_allow_html=True)
-        col2_3.number_input('', key=3)
-        col3_3.selectbox('',['mg/L', 'g/L', 'mg/m3','g/m3'], key=1)
-
-        ## Product concentration
-        col1_4, col2_4, col3_4 = st.beta_columns(3)
-        col1_4.markdown('')
-        col1_4.markdown("<div> <span class='highlight blue'>Product concentration<span class='bold'></div>", unsafe_allow_html=True)
-        col2_4.number_input('', key=4)
-        col3_4.selectbox('',['mg/L', 'g/L', 'mg/m3','g/m3'], key=2)
-
-    with st.beta_expander('Substrate Parameters'):
-        col1_6, col2_6, col3_6 = st.beta_columns(3)
-        col1_6.markdown('')
-        col1_6.markdown("<div> <span class='highlight blue'>'Biomass Yield (Yx/s)<span class='bold'></div>", unsafe_allow_html=True)
-        col2_6.number_input('', key=5)
-        col3_6.selectbox('',['mg X/ mg S'])
+        with st.beta_expander('Operation parameters'):
+            col1, col2, col3 = st.beta_columns(3)
+            col1.markdown('')
+            col1.markdown("<div> <span class='highlight blue'>Simulation Time <span class='bold'></div>", unsafe_allow_html=True)
+            time = col2.number_input('')
+            col3.selectbox('',['hours', 'minutes'])
             
 
-        col1_7, col2_7, col3_7, col4_7 = st.beta_columns(4)
-        col1_7.markdown('')
-        col1_7.markdown("<div> <span class='highlight blue'>Max growth rate, umax<span class='bold'></div>", unsafe_allow_html=True)
-        col2_7.number_input('', key=6)
-        col3_7.selectbox('',['L/hour'])
-        col4_7.markdown('')
-        col4_7.markdown('')
-        col4_7.checkbox('umax temperature correlation')
-
-        col1_8, col2_8, col3_8, col4_8 = st.beta_columns(4)
-        col1_8.markdown('')
-        col1_8.markdown("<div> <span class='highlight blue'>'Decay constant, b'<span class='bold'></div>", unsafe_allow_html=True)
-        col2_8.number_input('', key=7)
-        col3_8.selectbox('',['L/hour'] , key=1)
-        col4_8.markdown('')
-        col4_8.markdown('')
-        col4_8.checkbox('b temperature correlation')
+        #operating conditions
+            col1_1, col2_1, col3_1 = st.beta_columns(3)
+            col1_1.markdown('')
+            col1_1.markdown("<div> <span class='highlight blue'>Steptime, deltaT <span class='bold'></div>", unsafe_allow_html=True)
+            delta_t = col2_1.number_input('', key=1)
+            col3_1.selectbox('',['hours', 'minutes'], key=1)
         
-    with st.beta_expander('Product Parameters'):
-        col1_9, col2_9, = st.beta_columns(2)
+        #Initial concentration
+        with st.beta_expander('Initial concentrations'):
+            col1_2, col2_2, col3_2 = st.beta_columns(3)
+            col1_2.markdown('')
+            col1_2.markdown("<div> <span class='highlight blue'>Biomass concentration<span class='bold'></div>", unsafe_allow_html=True)
+            biomass_concentration= col2_2.number_input('', key=2)
+            col3_2.selectbox('',['mg/L', 'g/L', 'mg/m3','g/m3'])
+
+            ## substrate concenration
+
+            col1_3, col2_3, col3_3 = st.beta_columns(3)
+            col1_3.markdown('')
+            col1_3.markdown("<div> <span class='highlight blue'>Substrate concentration<span class='bold'></div>", unsafe_allow_html=True)
+            substate_concentration= col2_3.number_input('', key=3)
+            col3_3.selectbox('',['mg/L', 'g/L', 'mg/m3','g/m3'], key=1)
+
+            ## Product concentration
+            col1_4, col2_4, col3_4 = st.beta_columns(3)
+            col1_4.markdown('')
+            col1_4.markdown("<div> <span class='highlight blue'>Product concentration<span class='bold'></div>", unsafe_allow_html=True)
+            product_concentration=col2_4.number_input('', key=4)
+            col3_4.selectbox('',['mg/L', 'g/L', 'mg/m3','g/m3'], key=2)
+
+        with st.beta_expander('Substrate Parameters'):
+            col1_6, col2_6, col3_6 = st.beta_columns(3)
+            col1_6.markdown('')
+            biomass_yield= col1_6.markdown("<div> <span class='highlight blue'>'Biomass Yield (Yx/s)<span class='bold'></div>", unsafe_allow_html=True)
+            col2_6.number_input('', key=5)
+            col3_6.selectbox('',['mg X/ mg S'])
+                
+
+            col1_7, col2_7, col3_7, col4_7 = st.beta_columns(4)
+            col1_7.markdown('')
+            col1_7.markdown("<div> <span class='highlight blue'>Max growth rate, umax<span class='bold'></div>", unsafe_allow_html=True)
+            mumax=col2_7.number_input('', key=6)
+            col3_7.selectbox('',['L/hour'])
+            col4_7.markdown('')
+            col4_7.markdown('')
+            col4_7.checkbox('umax temperature correlation')
+
+            col1_8, col2_8, col3_8, col4_8 = st.beta_columns(4)
+            col1_8.markdown('')
+            col1_8.markdown("<div> <span class='highlight blue'>'Decay constant, b'<span class='bold'></div>", unsafe_allow_html=True)
+            decay_constant=col2_8.number_input('', key=7)
+            col3_8.selectbox('',['1/hour'] , key=1)
+            col4_8.markdown('')
+            col4_8.markdown('')
+            col4_8.checkbox('b temperature correlation')
+
+            col1_9, col2_9, col3_9= st.beta_columns(3)
+            col1_9.markdown('')
+            col1_9.markdown("<div> <span class='highlight blue'> Half saturation concentration, Ks <span class='bold'></div>", unsafe_allow_html=True)
+            Ks=col2_9.number_input('', key=8)
+            col3_9.selectbox('',['mg/L'] , key=1)
         
+            col1_10, col2_10, col3_10= st.beta_columns(3)
+            col1_10.markdown('')
+            col1_10.markdown("<div> <span class='highlight blue'> Substrate maintenance coefficient, mS <span class='bold'></div>", unsafe_allow_html=True)
+            mS=col2_10.number_input('', key=9)
+            col3_10.selectbox('',['1/hours'] , key=3)
 
-    
+        #Product Parameters
+        with st.beta_expander('Product Parameters'):
+            col1_11, col2_11, col3_11= st.beta_columns(3)
+            col1_11.markdown('')
+            col1_11.markdown("<div> <span class='highlight blue'> Product Type <span class='bold'></div>", unsafe_allow_html=True)
+            col2_11.radio('',['Growth Associated', 'Non-growth Associated', 'Mixed'])
+            kpg=col3_11.number_input('Kpg', key=3)
+            kpng=col3_11.number_input('Kpng', key=3)
 
+    #Product yied
+            col1_12, col2_12, col3_12= st.beta_columns(3)
+            col1_12.markdown('')
+            col1_12.markdown("<div> <span class='highlight blue'> Product Yield, Ypl/s <span class='bold'></div>", unsafe_allow_html=True)
+            product_yield=col2_12.number_input('', key=10)
+            col3_12.selectbox('',['mg P /mg S'], key=3)
+
+    # Product maintenance
+            col1_12, col2_12, col3_12= st.beta_columns(3)
+            col1_12.markdown('')
+            col1_12.markdown("<div> <span class='highlight blue'> Product maintenance coefficient, mP <span class='bold'></div>", unsafe_allow_html=True)
+            product_maintenance=col2_12.number_input('', key=11)
+            col3_12.selectbox('', ['1/hours'], key=4)
+
+
+    if st.button("Simulate"):
+        def dx_dt(x, t, mu, xmax):
+            '''Logistic equation (Chandrashekar et al 1999)'''
+            return mu* x* (1- x/xmax)
+
+    # mu = 1. 
+    # xmax = 30.
+    # x0 = 10.
+        t = np.linspace(0., int(time),  int(delta_t))
+        N = odeint(dx_dt, biomass_concentration, t, args=(mumax, (biomass_concentration+10
+    )))
+        fig = px.line(x=t, y=N)
+        fig.update_layout(title='IRR', autosize=False,
+                  width=800, height=800,
+                  margin=dict(l=40, r=40, b=40, t=40)
+)
+        st.plotly_chart(fig)
+
+    dataset = pd.DataFrame({'X': t, 'Y': N.ravel()}, columns=['X', 'Y'])
+
+    st.write(ols.get_residuals(dataset, 'Y'))
+
+    st.dataframe(dataset)
     
+    # Simulations
+
+    # ########Functions###############################
+
+    # Define a function which calculates the derivative
+    
+   
+
+if __name__ == "__main__":
+    main()
+
+
+
+            
+        
+    #####Caalls######################
+    # 
+
+   
 
     
 
---
+++ ols.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+import matplotlib.pyplot as plt
+import plotly.express as px
+import streamlit as st
+
+
+#create dataset
+df = pd.DataFrame({'rating': [90, 85, 82, 88, 94, 90, 76, 75, 87, 86],
+                   'points': [25, 20, 14, 16, 27, 20, 12, 15, 14, 19]})
+def get_residuals(dat, axis):
+    model = ols('X ~ Y', data=dat).fit()
+    stud_res = model.outlier_test()
+    x= dat[axis]
+    y= stud_res['student_resid']
+    plt.scatter(x, y)
+    plt.axhline(y=0, color='black', linestyle='--')
+    plt.xlabel('Points')
+    plt.ylabel('Studentized Residuals')
+    
+    fig_1 = px.scatter(x=x, y=y)
+    fig_1.update_layout(title='Studentized residual Plot', autosize=False,
+                  width=800, height=800,
+                  margin=dict(l=40, r=40, b=40, t=40), shapes=[dict(type= 'line',yref= 'y', y0= 0, y1= 0, xref= 'paper', x0= 0, x1= 14)])
+                  
+    return st.plotly_chart(fig_1)
+
+
+
